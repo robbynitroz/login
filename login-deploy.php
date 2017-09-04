fb97529eee031c818be427d7d39fd76fda0dabb3
Developer <davit.developer@gmail.com>
2017-09-04 18:11:02 +0400

    Merge

--- login-deploy.php
+++ login-deploy.php
@@ -49,7 +49,7 @@ if (isset($_GET['secretword'])=='yes1234567899877451wwdsfsdf549fds'){
     $allowed = true;
 }
 
-if (!$allowed) {
+if (isset($allowed)) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
