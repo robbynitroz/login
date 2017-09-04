8a7dfa03ebf2783eb744e9beb90cb96f65a1449d
Developer <davit.developer@gmail.com>
2017-09-04 18:13:14 +0400

    Merge

--- login-deploy.php
+++ login-deploy.php
@@ -43,13 +43,13 @@ foreach ($allowed_ips as $allow) {
         break;
     }
 }*/
+$allowed=false;
 
-
-if (isset($_GET['secretword'])=='yes1234567899877451wwdsfsdf549fds'){
+if (isset($_GET['secretword'])==='yes1234567899877451wwdsfsdf549fds'){
     $allowed = true;
 }
 
-if (isset($allowed)) {
+if ($allowed===true) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
