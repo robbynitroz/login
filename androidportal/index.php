a493a89409e62877922b98aeb91bea47ab688880
Developer <davit.developer@gmail.com>
2017-09-26 00:44:51 +0400

    Merge

--- androidportal/index.php
+++ androidportal/index.php
@@ -1,15 +1,16 @@
 <?php
 
 
-if(isset($_GET['key'])) {
+/*if(isset($_GET['key'])) {
 
     http_response_code(204);
 }else{
-    http_response_code(404);
+    http_response_code(200);
 
-}
+}*/
 
 
-
+http_response_code(404);
+header("HTTP/1.1 404 No Content")
 
 ?>
