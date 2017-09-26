da0f70b41b1c57ab15ab77494f32d844061dd50a
Developer <davit.developer@gmail.com>
2017-09-27 00:47:30 +0400

    Merge

--- androidportal/index.php
+++ androidportal/index.php
@@ -1,21 +1,23 @@
 <?php
 
 
-if($_GET['give_access'] or $_COOKIE["give_access"]){
+if ($_GET['give_access']) {
 
 
-        setcookie("give_access", "Yes", time() + (60 * 5), "/"); // 86400 = 1 day
-        http_response_code(204);
+    setcookie("give_access", "Yes", time() + (60 * 5), "/"); // 86400 = 1 day
+    header('location:http://login.com');
 
-        header('location:http://login.com');
+} else {
 
-}else{
-
-    $address = 'http://'.$_SERVER['SERVER_NAME'].'/welcome.php';
+    $address = 'http://' . $_SERVER['SERVER_NAME'] . '/welcome.php';
 
     header("location: $address ");
 }
 
+
+if ($_COOKIE["give_access"]) {
+    http_response_code(204);
+}
 
 
 //http_response_code(204);
